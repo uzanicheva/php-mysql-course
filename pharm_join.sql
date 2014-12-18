667d0a2f810728f1eb67ddbd3d572c6329222a67
uzanicheva <uzanicheva@users.noreply.github.com>
2014-12-18 12:18:17 +0200

    Delete pharm_join.sql

--- pharm_join.sql
+++
@@ -1,4 +0,0 @@
-SELECT one.PharmacyID, one.Name, one.Address, two.EmployeeID, two.Name, two.Surname, two.Age, two.Gender, two.IDNumber, two.Phone, three.DrugID, three.ExpDate, three.Distributor, three. Price , three.Quantity 
-FROM Pharm.Pharmacies AS one
-JOIN Pharm.Employees AS two ON two.PharmacyID = three.PharmacyID
-JOIN Pharm.Drugs AS three ON three.PharmacyID = two.PharmacyID
